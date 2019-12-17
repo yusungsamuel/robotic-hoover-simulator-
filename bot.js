3d5e2124ce98e00e06c415c9754117e49d8c8111
yusungsamuel <ssyyu@ucdavis.edu>
2019-12-16 22:05:55 -0800

    update createRoom function to also map dirt patches into room

--- bot.js
+++ bot.js
@@ -3,63 +3,73 @@ var fs = require("fs")
 var Table = require('cli-table2');
 
 //function to map out the dirt patches in the room
-dirtDistribution = (arr) =>{
+dirtDistribution = (arr) => {
     let dirtObj = {}
     //check to see if there are dirt patches
-    if(arr.length) {
+    if (arr.length) {
         arr.forEach((data) => {
             let position = data.split(" ")
-            if(dirtObj[position[0]]){
+            if (dirtObj[position[0]]) {
                 dirtObj[position[0]][position[1]] = true
             }
             else {
                 dirtObj[position[0]] = {}
                 dirtObj[position[0]][position[1]] = true
             }
-            
+
         })
     }
-    console.log(dirtObj)
+    return (dirtObj)
 }
 
 //function to create the room grid, where it takes two parameter w = width and l = length
-createRoom = (w, l) => {
+createRoom = (w, l, dirt) => {
     //create  new table object using cli-table2
-    let table = new Table()    
-    let row = []
-    for (let i = 0; i< w; i++){
-        row.push("")
-    }
-    for (let j = 0; j< l; j++){
+    let table = new Table()
+
+    let dirtMap = dirtDistribution(dirt)
+
+    for (let i = 0; i < w; i++) {
+        let row = []
+        for (let j = 0; j < l; j++) {
+            if (dirtMap[i] && dirtMap[i][j]) {
+
+                row.push("x")
+
+            }
+            else {
+                row.push("")
+            }
+        }
         table.push(row)
     }
-   
     console.log(table.toString())
 }
 
 
 readFile = () => {
-    fs.readFile("input.txt", "utf8", (err, data)=>{
+    fs.readFile("input.txt", "utf8", (err, data) => {
         if (err) throw err;
-        
+
         //putting data read from input txt file into an array and separating them line by line
         let dataArr = data.split("\n")
-        
+
         // the first line of data contain the dimension of the room
         const dimension = dataArr[0].split(" ")
-        
+
         //extracting the length and width of the room and converting the data to a number
         const x = parseInt(dimension[0])
         const y = parseInt(dimension[1])
-        
-        // createRoom(x, y)
+
 
         //second line contains the hoover position
         let hooverPosition = dataArr[1]
 
-        let dirtPatch = dataArr.slice(2, dataArr.length-1)
+        let dirtPatch = dataArr.slice(2, dataArr.length - 1)
 
-        dirtDistribution(dirtPatch)
+        // dirtDistribution(dirtPatch)
+        createRoom(x, y, dirtPatch)
+
 
         //last line contain the instruction
         let instruction = dataArr[dataArr.length - 1]
