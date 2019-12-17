9995aacdf810ee9863aeb86d7731d0de49bd91b5
yusungsamuel <ssyyu@ucdavis.edu>
2019-12-16 20:40:19 -0800

    parsing out room dimension, hoover position and instruction into individual variables

--- bot.js
+++ bot.js
@@ -3,8 +3,23 @@ var fs = require("fs")
 readFile = () => {
     fs.readFile("input.txt", "utf8", (err, data)=>{
         if (err) throw err;
-        console.log(data)
+        
+        //putting data read from input txt file into an array and separating them line by line
+        let dataArr = data.split("\n")
+        
+        // the first line of data contain the dimension of the room
+        let dimension = dataArr[0]
+
+        //second line contains the hoover position
+        let hooverPosition = dataArr[1]
+
+        //last line contain the instruction
+        let instruction = dataArr[dataArr.length - 1]
     })
 }
+
+
+
+
 
 readFile();
